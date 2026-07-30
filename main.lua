bf07d70044c26cc1adf483a5aeef38afd4a8b970
PapierZylinder <45166325+PapierZylinder@users.noreply.github.com>
2026-07-30 22:41:01 +0200

    doesCollide in seine eigene .lua gepackt

--- main.lua
+++ main.lua
@@ -6,65 +6,27 @@ local player
 local player2
 local ball
 local score
-local state
-
-local doesCollide
-
-function doesCollide(_ball, _player)
-	return _ball.x < _player.x + 20
-		and _ball.x + _ball.size > _player.x
-		and _ball.y < _player.y + 200
-		and _ball.y + _ball.size > _player.y
-end
 
 function love.load()
-	state = "menu"
 	player = Player.new()
-	player2 = Player.new(true, 760, 200, "up", "down")
-	score = Score.new()
+	player2 = Player.new(love.graphics.getWidth() - 40, love.graphics.getHeight() / 2 - 50)
 	ball = Ball.new()
-	ball:reset()
+	score = Score.new()
 end
 
 function love.update(dt)
-	if state == "menu" then
-		if love.keyboard.isDown("space") then
-			state = "play"
-		end
-	end
-
-	if state == "play" then
-		player:update(dt)
-		player2:update(dt, ball.y)
-		ball:update(dt)
-		if doesCollide(ball, player) then
-			ball.x = player.x + ball.size
-			ball.vx = -ball.vx
-		end
-		if doesCollide(ball, player2) then
-			ball.x = player2.x - ball.size
-			ball.vx = -ball.vx
-		end
-		score:update(ball.x, ball)
-		if love.keyboard.isDown("escape") then
-			ball:reset()
-		end
-	end
+	player:update(dt)
+	player2:update(dt, "up", "down")
+	ball:update(dt, player, player2)
+	score:update(ball)
 end
 
 function love.draw()
-	love.graphics.setDefaultFilter("nearest", "nearest")
-	if state == "menu" then
-		love.graphics.print("Press Space to Play", 200, 200, 0, 5, 5)
-	end
-
-	if state == "play" then
-		love.graphics.line(400, 0, 400, 600)
-		love.graphics.circle("line", 400, 300, 70)
-
-		score:draw()
-		ball:draw()
-		player:draw()
-		player2:draw()
-	end
+	love.graphics.rectangle("fill", love.graphics.getWidth() / 2, 0, 1, love.graphics.getHeight())
+	love.graphics.rectangle("fill", 0, love.graphics.getHeight() / 2, love.graphics.getWidth(), 1)
+	love.graphics.circle("line", love.graphics.getWidth() / 2, love.graphics.getHeight() / 2, 40)
+	ball:draw()
+	player:draw()
+	player2:draw()
+	score:draw()
 end
