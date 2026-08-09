c1da75714384787a4e4bbc1cd2749ec09ac5896a
PapierZylinder <45166325+PapierZylinder@users.noreply.github.com>
2026-08-09 20:52:31 +0200

    Debugger in conf.lua gepackt

--- conf.lua
+++ conf.lua
@@ -1,4 +1,14 @@
+local IS_DEBUG = os.getenv("LOCAL_LUA_DEBUGGER_VSCODE") == "1" and arg[2] == "debug"
+if IS_DEBUG then
+	require("lldebugger").start()
+
+	function love.errorhandler(msg)
+		error(msg, 2)
+	end
+end
+
 function love.conf(t)
 	t.title = "Pong"
 	t.version = "11.5"
+	t.console = false
 end
